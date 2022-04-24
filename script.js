83783e09540197ee7e49387b564ba36be7e288bc
ShivaRK17 <99678125+ShivaRK17@users.noreply.github.com>
2022-04-24 23:49:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNav(){
+    document.getElementById('mySidenav').style.width="300px";
+}
+function closeNav(){
+    document.getElementById('mySidenav').style.width="0px";
+}
+// var slider = 0;
+// function slidett(){
+//     if (slider==0) {
+//         document.getElementById('ttimg').style.width="80vw";
+//         slider=1;
+//     }
+//     else{
+//         document.getElementById('ttimg').style.width="0vw";
+//         slider=0;
+//     }
+// }
